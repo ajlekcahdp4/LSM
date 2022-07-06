b89ed1a7796afd47c385200260f53525fa0007b1
ajlekcahdp4 <alex.rom23@mail.ru>
2022-07-06 16:43:50 +0300

    make some functions public

--- include/lsm.h
+++ include/lsm.h
@@ -42,7 +42,17 @@ enum ERRORS
     ERROR_WRONG_PATH_OR_NAME_FOR_OUTPUT_FILE
 };
 
+
+struct lsm_t *LinearCalc (struct input *INP);
+struct lsm_t *PolinomCalc (struct input *INP, size_t deg);
+struct lsm_t *ExpCalc (struct input *INP);
+
 int LinearLsmCalc (char *inputname, char *outname, char *xlabel, char *ylabel, enum format fmt);
 int PolinomLsmCalc (int deg, char *inputname, char *outname, char *xlabel, char *ylabel, enum format fmt);
 int ExpLsmCalc (char *inputname, char *outname, char *xlabel, char *ylabel, enum format fmt);
+
+struct lsm_t *LinearCalc (struct input *INP);
+int PolinomLsmPrint (struct lsm_t *POL, char *outname);
+int ExpLsmPrint (struct lsm_t *EXP, char *outname);
+
 #endif
